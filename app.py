e55187ff933a1088a22f46eab90c694590cfc85c
funkeyy261-debug <poudeldinesh261@gmail.com>
2026-08-25 23:14:05 +0545

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ WHATSAPP_LINK = "https://wa.me/9779800000000"
 def get_db():
     conn = sqlite3.connect(DB)
     conn.row_factory = sqlite3.Row
+    try:
+        conn.execute("ALTER TABLE requests ADD COLUMN price TEXT")
+    except:
+        pass
     return conn
 
 
