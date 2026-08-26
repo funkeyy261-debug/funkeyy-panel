86e21a7b2f92a80d20b7fa02d4c760f44e154c1a
funkeyy261-debug <poudeldinesh261@gmail.com>
2026-08-26 09:18:01 +0545

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ ADMIN_PASSWORD = "20622"
 
 TELEGRAM_LINK = "https://t.me/funkeyy_dinesh_bot"
 WHATSAPP_LINK = "https://wa.me/9779765936556"
-
+TELEGRAM_BOT_TOKEN = "8852511502:AAGlo6IPNgy2NFQiVzPzOMuyqcJO93Vps1c"
 
 # ================= DATABASE =================
 
